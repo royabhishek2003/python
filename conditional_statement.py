2f12d4f4fcdf0acf1cd329932e9f0d2c2a01aed0
royabhishek2003 <kishanabhishek2003@gmail.com>
2026-04-28 19:11:24 +0530

    loops in python

--- conditional_statement.py
+++ conditional_statement.py
@@ -1,18 +1,28 @@
 #  conditional statament 
 a=10
 
-if a>5:
-    print("a is greater than 5 ")
+# if a>5:
+#     print("a is greater than 5 ")
 
-else:    print("a is less than or equal to 5")
+# else:    print("a is less than or equal to 5")
 #  mutiple conditions 
-b=20
-if a>5 and b>15:
-    print("a is greater than 5 and b is greater than 15")
-elif a>5 or b>15:
-    print("a is less than or equal to 5 or b is less than or equal to 15")
-elif not(a>5 and b>15):
-    print("a is less than or equal to 5 and b is less than or equal to 15")
-else:
-    print("a is less than or equal to 5 and b is less than or equal to 15")
+# b=20
+# if a>5 and b>15:
+#     print("a is greater than 5 and b is greater than 15")
+# elif a>5 or b>15:
+#     print("a is less than or equal to 5 or b is less than or equal to 15")
+# elif not(a>5 and b>15):
+#     print("a is less than or equal to 5 and b is less than or equal to 15")
+# else:
+#     print("a is less than or equal to 5 and b is less than or equal to 15")
     
+
+# take character input 
+char = input("Enter your gender (M/F): ")
+if char =='M'  or char =='m':
+    print("You are male")
+elif char =='F' or char=='f':
+    print("You are female")
+else:
+    print("You are Hizra")
+
---
+++ loops.py
@@ -0,0 +1,22 @@
+# for loop 
+# Range Function -> range(start, stop+1, step) defa
+
+for i in range(5):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
+    
+for i in range (10, 0,-1):
+    print(i)
+    
+for i in range(0, 11, 2):
+    print(i)
+
+a= range(1,10,2)
+
+for i in a:
+    print(i)
+
+
+
